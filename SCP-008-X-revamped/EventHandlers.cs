29099c7a7f8aed5ec59ba4e4ef4b4d7ad761f980
Alexey Strukov <oplkill@gmail.com>
2024-02-11 10:20:13 +0300

    First release

---
+++ SCP-008-X-revamped/EventHandlers.cs
@@ -0,0 +1,31 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using Exiled.Events.EventArgs.Player;
+using InventorySystem;
+
+namespace SCP_008_X_revamped
+{
+    internal class EventHandlers
+    {
+        public void OnHurting(HurtingEventArgs ev)
+        {
+            var player = ev.Player;
+            if (!player.IsHuman || player.IsTutorial)
+                return;
+
+            if (ev.DamageHandler.Type == Exiled.API.Enums.DamageType.Crushed)
+                return;
+
+            bool isDamageDeadly = !((player.Health - ev.Amount) > 0.0);
+
+            if (!isDamageDeadly)
+                return;
+
+            ev.IsAllowed = false;
+
+            player.Inventory.ServerDropEverything();
+            player.Role.Set(PlayerRoles.RoleTypeId.Scp0492);
+        }
+    }
+}
---
+++ SCP-008-X-revamped/Plugin.cs
@@ -0,0 +1,48 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using Exiled.API.Features;
+using System;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace SCP_008_X_revamped
+{
+    public class SCP008X : Plugin<Config, Translations>
+    {
+        public static SCP008X Instance;
+        public override Version Version => new Version(0, 0, 1);
+        public override string Author => "Oplkill";
+        public override string Name => "SCP-008-X-revamped";
+        public override string Prefix => "SCP-008-X";
+
+        private EventHandlers _eventHandler;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            RegisterEvents();
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            UnRegisterEvents();
+            Instance = null;
+            base.OnDisabled();
+        }
+
+        private void RegisterEvents()
+        {
+            _eventHandler = new EventHandlers();
+
+            Player.Hurting += _eventHandler.OnHurting;
+        }
+
+        private void UnRegisterEvents()
+        {
+            Player.Hurting -= _eventHandler.OnHurting;
+
+            _eventHandler = null;
+        }
+    }
+}
